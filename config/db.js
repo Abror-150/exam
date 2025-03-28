53b9133908ea1c7b60823c9a3744482cf2aa0dbb
Ikromxon <ikromkhanshukrullayev03022005@gmail.com>
2025-03-28 17:11:55 +0500

    force true added

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ async function connectedDb() {
     await db.authenticate();
     console.log('connected db');
     require('../models/connections');
-    // await db.sync({ alter: true });
+    // await db.sync({ force: true });
     // console.log('ulandi ');
   } catch (error) {
     console.log(error);
